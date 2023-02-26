43b83d55d0a1299d9aeea0ec59532445562b135e
49EHyeon42 <49ehyeon42@gmail.com>
2023-02-26 17:24:22 +0900

    init: project

---
+++ src/main/java/dev/ehyeon/ehyeon/EHyeonProjectApplication.java
@@ -0,0 +1,12 @@
+package dev.ehyeon.ehyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHyeonProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EHyeonProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/dev/ehyeon/ehyeon/EHyeonProjectApplicationTests.java
@@ -0,0 +1,12 @@
+package dev.ehyeon.ehyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHyeonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
